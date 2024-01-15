dc642e3a959ec8da2faa842fe4df32fd811fb5c4
DariaBandukova <dariabandukova@gmail.com>
2024-01-15 17:06:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button1 = findViewById<MaterialButton>(R.id.button1)
+        button1.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+        val button2 = findViewById<MaterialButton>(R.id.button2)
+        button2.setOnClickListener {
+            val displayIntent2 = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent2)
+        }
+        val button3 = findViewById<MaterialButton>(R.id.button3)
+        button3.setOnClickListener {
+            val displayIntent3 = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent3)
+        }
+    }
+}
